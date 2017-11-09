0207bb0c959bd7faac52d305137088ad0e388b71
lmucassi <lindasmucassi@gmai.com>
2017-11-10 00:22:06 +0200

    updates landing page background

--- index.php
+++ index.php
@@ -9,18 +9,38 @@
 <html lang="en">
 <head>
     <title>MATCHA_</title>
+
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="student@wethinkcode">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="styles/headers.css">
     <link rel="stylesheet" type="text/css" href="styles/logger.css">
+    <link rel="shortcut icon" href="../favicon.ico">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
 <body>
     <!--landing login register header -->
     <?php include('headers/landing.php') ?>
+    <div class="carousel slide carousel-fade" data-ride="carousel">
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+            </div>
+            <div class="item">
+            </div>
+            <div class="item">
+            </div>
+        </div>
+    </div>
     <?php include('headers/footer.php') ?>
 </body>
 </html>
