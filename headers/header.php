598e2182f0b81b7d8027ac07cbcef0301d77784e
lmucassi <lindasmucassi@gmai.com>
2017-11-09 23:11:10 +0200

    updated header file for logged in uders

--- headers/header.php
+++ headers/header.php
@@ -27,32 +27,32 @@
         $logout = 'Active';
     }
 ?>
-
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
+        <!-- collapsed button for small size screen -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">WebSiteName</a>
+            <!-- website Name -->
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-heart-empty"></span>Matcha</a>
         </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+        <!-- navigation items -->
+        <div class="collapse navbar-collapse ml-auto" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li class=""><a href="#"><span class="glyphicon glyphicon-heart-empty"></span>matcha</a></li>
                 <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-bell"></span><span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="#">Page 1-1</a></li>
                         <li><a href="#">Page 1-2</a></li>
                         <li><a href="#">Page 1-3</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Page 2</a></li>
-                <li><a href="#">Page 3</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-out"></span></a></li>
             </ul>
         </div>
     </div>
