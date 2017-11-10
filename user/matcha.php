72106e888e7a0910e92407d5756ddf3dad85a8e3
lmucassi <lindasmucassi@gmai.com>
2017-11-10 05:17:59 +0200

    suggestions

--- user/matcha.php
+++ user/matcha.php
@@ -25,6 +25,41 @@
         <!-- suggestion to user -->
         <div class="sugg-colum">
             <h3>Here are possible matches for you</h3>
+            <div class="wrapper">
+                <div class="box a"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+                    <input type="checkbox" id="pure-toggle" hidden/>
+                    <label id="card-user" class="pure-toggle" for="pure-toggle">
+                        <div class="header">
+                        </div>
+                        <div class="avatar">
+                            <img src="http://wpidiots.com/html/writic/red-writic-template/css/img/demo-images/avatar1.jpg" alt="...">
+                        </div>
+                        <div class="text">
+                            <h3>John Doe</h3>
+                            <p>some information about the user</p>
+                        </div>
+
+                        <div class="skills">
+                            <h3>Skills</h3>
+                            <i class="fa fa-html5 fa-2x"></i>
+                            <i class="fa fa-css3 fa-2x"></i>
+                            <i class="fa fa-github fa-2x"></i>
+                        </div>
+
+                        <div class="social">
+                            <i class="fa fa-codepen" alt="codepen" title="codepen"></i>
+                            <i class="fa fa-facebook" alt="facebook" title="facebook"></i>
+                            <i class="fa fa-google" alt="google" title="google"></i>
+                            <i class="fa fa-twitter" alt="twitter" title="twitter"></i>
+                            <i class="fa fa-youtube" alt="youtube" title="youtube"></i>
+                        </div>
+                </div></div>
+                <div class="box b">B</div>
+                <div class="box c">C</div>
+                <div class="box d">D</div>
+                <div class="box e">E</div>
+                <div class="box f">F</div>
+            </div>
         </div>
         <!-- just a vertical line -->
         <div class=""></div>
