87247c0463889268c4438bf783e2cd78cc5fe7ea
lmucassi <lindasmucassi@gmai.com>
2017-11-10 05:24:38 +0200

    suggestion

---
+++ user/suggested.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmucassi
+ * Date: 2017/11/10
+ * Time: 5:19 AM
+ */?>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <input type="checkbox" id="pure-toggle" hidden/>
+    <label id="card-user" class="pure-toggle" for="pure-toggle">
+        <div class="header">
+        </div>
+        <div class="avatar">
+            <img src="http://wpidiots.com/html/writic/red-writic-template/css/img/demo-images/avatar1.jpg" alt="...">
+        </div>
+        <div class="text">
+            <h3>John Doe</h3>
+            <p>some information about the user</p>
+        </div>
+
+        <div class="skills">
+            <h3>Skills</h3>
+            <i class="fa fa-html5 fa-2x"></i>
+            <i class="fa fa-css3 fa-2x"></i>
+            <i class="fa fa-github fa-2x"></i>
+        </div>
+
+        <div class="social">
+            <i class="fa fa-codepen" alt="codepen" title="codepen"></i>
+            <i class="fa fa-facebook" alt="facebook" title="facebook"></i>
+            <i class="fa fa-google" alt="google" title="google"></i>
+            <i class="fa fa-twitter" alt="twitter" title="twitter"></i>
+            <i class="fa fa-youtube" alt="youtube" title="youtube"></i>
+        </div>
+
