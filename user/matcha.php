9583db7223ceb2e09b1b7a213b9476d97ee83487
lmucassi <lindasmucassi@gmai.com>
2017-11-10 05:34:38 +0200

    createdsearch

--- user/matcha.php
+++ user/matcha.php
@@ -32,6 +32,9 @@
                 <div><?php include('suggested.php') ?></div>
                 <div><?php include('suggested.php') ?></div>
                 <div><?php include('suggested.php') ?></div>
+                <div><?php include('suggested.php') ?></div>
+                <div><?php include('suggested.php') ?></div>
+                <div><?php include('suggested.php') ?></div>
             </div>
         </div>
         <!-- just a vertical line -->
@@ -39,35 +42,9 @@
         <!-- search and results -->
         <div class="sidebar">
             <!-- search and filter -->
-            <div class="">
-                <div class="">
-                    <div class="col-xs-8 col-xs-offset-2">
-                        <div class="input-group">
-                            <div class="input-group-btn search-panel">
-                                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                    <span id="search_concept">Filter by</span> <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#contains">Gender</a></li>
-                                    <li><a href="#its_equal">Age</a></li>
-                                    <li><a href="#greather_than">close location</a></li>
-                                    <li><a href="#less_than"></a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#all">Anything</a></li>
-                                </ul>
-                            </div>
-                            <input type="hidden" name="search_param" value="all" id="search_param">
-                            <input type="text" class="form-control" name="x" placeholder="Search term...">
-                            <span class="input-group-btn">
-                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?php include('search.php') ?>
         </div>
     </div>
-
     <!-- footer -->
     <?php include('../headers/footer.php')?>
 </body>
---
+++ user/search.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmucassi
+ * Date: 2017/11/10
+ * Time: 5:32 AM
+ */?>
+<!-- search and results -->
+<div class="">
+    <div class="">
+        <div class="col-xs-8 col-xs-offset-2">
+            <div class="input-group">
+                <div class="input-group-btn search-panel">
+                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                        <span id="search_concept">Filter by</span> <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#contains">Gender</a></li>
+                        <li><a href="#its_equal">Age</a></li>
+                        <li><a href="#greather_than">close location</a></li>
+                        <li><a href="#less_than"></a></li>
+                        <li class="divider"></li>
+                        <li><a href="#all">Anything</a></li>
+                    </ul>
+                </div>
+                <input type="hidden" name="search_param" value="all" id="search_param">
+                <input type="text" class="form-control" name="x" placeholder="Search term...">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
