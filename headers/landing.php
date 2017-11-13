a33674e990af0a8efa1f975969b5a4a3bb4613fc
lmucassi <lindasmucassi@gmai.com>
2017-11-13 16:11:48 +0200

    footerFixed

--- headers/landing.php
+++ headers/landing.php
@@ -40,31 +40,31 @@
                     <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                         <!-- register FullName -->
                         <div class="form-group">
-                            <input type="text" class="form-control" id="" placeholder="Fullname" name="reg_name" required>
+                            <input type="text" class="form-control" id="" placeholder="Fullname" name="" required>
                         </div>
                         <!-- register username -->
                         <div class="form-group">
                             <label class="sr-only">Username</label>
-                            <input type="text" class="form-control" id="" placeholder="Username" name="reg_userlogin" required>
+                            <input type="text" class="form-control" id="" placeholder="Username" name="" required>
                         </div>
                         <!-- email address -->
                         <div class="form-group">
                             <label class="sr-only">Email</label>
-                            <input type="email" class="form-control" id="" placeholder="Email Address" name="reg_email" required>
+                            <input type="email" class="form-control" id="" placeholder="Email Address" name="" required>
                         </div>
                         <!-- password -->
                         <div class="form-group">
                             <label class="sr-only">Password</label>
-                            <input type="password" class="form-control" id="" placeholder="Password" name="reg_password_1" required>
+                            <input type="password" class="form-control" id="" placeholder="Password" name="" required>
                         </div>
                         <!-- confirm password -->
                         <div class="form-group">
                             <label class="sr-only">Password</label>
-                            <input type="password" class="form-control" id="" placeholder="Confirm Password" name="reg_password_2" required>
+                            <input type="password" class="form-control" id="" placeholder="Confirm Password" name="" required>
                         </div>
                         <!-- sign up button -->
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary btn-block" name="reg_user">Sign Up</button>
+                            <button type="submit" class="btn btn-primary btn-block" name="">Sign Up</button>
                         </div>
                     </form>
                 </div>
@@ -100,11 +100,11 @@
                     <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
                         <div class="form-group">
                             <label class="sr-only" for="exampleInputEmail2">Email address or Username</label>
-                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" name="log_userlogin" required>
+                            <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" name="" required>
                         </div>
                         <div class="form-group">
                             <label class="sr-only" for="exampleInputPassword2">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" name="log_password" required>
+                            <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" name="" required>
                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
                         </div>
                         <div class="form-group">
@@ -120,7 +120,7 @@
                 </div>
                 <div class="bottom text-center">
                     New here ? <a data-toggle="modal" data-target="#registerModal" href="#"><b>Join Us</b></a>
-                </div`>
+                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
--- index.php
+++ index.php
@@ -28,14 +28,12 @@
     <div class="carousel slide carousel-fade" data-ride="carousel">
         <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
-            <div class="item active">
-            </div>
-            <div class="item">
-            </div>
-            <div class="item">
-            </div>
+            <div class="item"></div>
+            <div class="item"></div>
+            <div class="item"></div>
         </div>
     </div>
+    <!-- footer -->
     <?php include('headers/footer.php') ?>
 </body>
 </html>
